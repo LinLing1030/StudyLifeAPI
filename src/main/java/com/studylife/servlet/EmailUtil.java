bd11448c5e43071de9b57a6dbed57200b7ea7a80
LINLING GUO <llguo10300@gmail.com>
2025-08-22 02:27:50 +0100

    fixes

--- src/main/java/com/studylife/servlet/EmailUtil.java
+++ src/main/java/com/studylife/servlet/EmailUtil.java
@@ -1,72 +1,43 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.studylife.servlet;
 
-import javax.mail.*;
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.Message.RecipientType;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import java.util.Properties;
 
-public final class EmailUtil {
-	  private static String p(String k, String d){ return System.getProperty(k, d); }
+public class EmailUtil {
+    public EmailUtil() {
+    }
 
-	  public static void sendEmail(String to, String subject, String text) throws Exception {
-	    String username = p("MAIL_USERNAME", "noreply@test.local");
-	    String password = p("MAIL_PASSWORD", "");
-	    String host     = p("MAIL_SMTP_HOST", "localhost");
-	    String port     = p("MAIL_SMTP_PORT", "3025");
-	    String from     = p("MAIL_FROM", username);
-
-	    Properties props = new Properties();
-	    props.put("mail.smtp.host", host);
-	    props.put("mail.smtp.port", port);
-	    props.put("mail.smtp.auth",            p("MAIL_SMTP_AUTH", "false"));
-	    props.put("mail.smtp.starttls.enable", p("MAIL_STARTTLS",  "false"));
-	    props.put("mail.smtp.ssl.enable",      p("MAIL_SSL",       "false"));
-
-	    Session session = Session.getInstance(props,
-	        "true".equals(props.getProperty("mail.smtp.auth"))
-	          ? new Authenticator() {
-	              @Override protected PasswordAuthentication getPasswordAuthentication() {
-	                return new PasswordAuthentication(username, password);
-	              }
-	            }
-	          : null);
-
-	    Message msg = new MimeMessage(session);
-	    msg.setFrom(new InternetAddress(from));
-	    msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-	    msg.setSubject(subject);
-	    msg.setText(text);
-	    Transport.send(msg);
-	  }
-
-	  public static void sendHtmlEmail(String to, String subject, String html) throws Exception {
-	    String username = p("MAIL_USERNAME", "noreply@test.local");
-	    String password = p("MAIL_PASSWORD", "");
-	    String host     = p("MAIL_SMTP_HOST", "localhost");
-	    String port     = p("MAIL_SMTP_PORT", "3025");
-	    String from     = p("MAIL_FROM", username);
-
-	    Properties props = new Properties();
-	    props.put("mail.smtp.host", host);
-	    props.put("mail.smtp.port", port);
-	    props.put("mail.smtp.auth",            p("MAIL_SMTP_AUTH", "false"));
-	    props.put("mail.smtp.starttls.enable", p("MAIL_STARTTLS",  "false"));
-	    props.put("mail.smtp.ssl.enable",      p("MAIL_SSL",       "false"));
-
-	    Session session = Session.getInstance(props,
-	        "true".equals(props.getProperty("mail.smtp.auth"))
-	          ? new Authenticator() {
-	              @Override protected PasswordAuthentication getPasswordAuthentication() {
-	                return new PasswordAuthentication(username, password);
-	              }
-	            }
-	          : null);
-
-	    MimeMessage msg = new MimeMessage(session);
-	    msg.setFrom(new InternetAddress(from));
-	    msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-	    msg.setSubject(subject);
-	    msg.setContent(html, "text/html; charset=UTF-8"); // ← HTML
-	    Transport.send(msg);
-	  }
-	}
+    public static void sendEmail(String toEmail, String subject, String messageText) throws MessagingException {
+        String fromEmail = "llguo10300@gmail.com";
+        String password = "ocfl oicq uity xiyu";
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("llguo10300@gmail.com", "ocfl oicq uity xiyu");
+            }
+        });
+        Message message = new MimeMessage(session);
+        ((Message)message).setFrom(new InternetAddress("llguo10300@gmail.com"));
+        ((Message)message).setRecipients(RecipientType.TO, InternetAddress.parse(toEmail));
+        ((Message)message).setSubject(subject);
+        ((Message)message).setText(messageText);
+        Transport.send(message);
+    }
+}
